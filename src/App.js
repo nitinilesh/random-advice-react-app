43ef62d98b77e68bdd8a5e9cc5ceffdccb96e496
Nilesh Patel <62102565+nitinilesh@users.noreply.github.com>
2026-02-02 20:35:45 +0000

    added count and a new component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
   }
   return (
     <div>
-      <h1>Hello world! {advice}</h1>
+      <h3 className="advice">{advice}</h3>
+
       <button onClick={getAdvice}>Get advice</button>
+      <div className="count">Advice No: {count}</div>
     </div>
   );
 }

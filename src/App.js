b6fe47ad4157c08b7c08081f55f1f387aa5d94bf
Nilesh Patel <62102565+nitinilesh@users.noreply.github.com>
2026-02-02 20:37:05 +0000

    added new component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
-function App() {
+export default function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
   async function getAdvice() {
@@ -10,13 +11,22 @@ function App() {
     setAdvice(data.slip.advice);
     setCount((count) => count + 1);
   }
+  useEffect(function () {
+    getAdvice();
+  }, []);
   return (
     <div>
       <h3 className="advice">{advice}</h3>
 
       <button onClick={getAdvice}>Get advice</button>
-      <div className="count">Advice No: {count}</div>
+      <Message value={count} />
     </div>
   );
 }
-export default App;
+function Message(props) {
+  return (
+    <>
+      <p className="count">{`You have read ${props.value} pieces of advice`}</p>
+    </>
+  );
+}
